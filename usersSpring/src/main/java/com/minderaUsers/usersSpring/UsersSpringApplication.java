54b1ed2c80cd737bd3c38b51c9c9a221206e9470
PedroBarge <pedro525tavares@gmail.com>
2024-01-17 18:37:16 +0000

    created UserSpring

---
+++ usersSpring/src/main/java/com/minderaUsers/usersSpring/UsersSpringApplication.java
@@ -0,0 +1,13 @@
+package com.minderaUsers.usersSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersSpringApplication.class, args);
+	}
+
+}
---
+++ usersSpring/src/test/java/com/minderaUsers/usersSpring/UsersSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minderaUsers.usersSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
