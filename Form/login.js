c599ee1ad9796bf67363edf642684cb9871d7e22
PedroBarge <pedro525tavares@gmail.com>
2024-01-31 12:26:52 +0000

    need to finnish js

---
+++ Form/login.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("loginForm");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+
+  
+});
