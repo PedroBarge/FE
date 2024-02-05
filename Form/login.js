89e496c6f6d81bd9e014118c4a56094651ae5e7e
PedroBarge <pedro525tavares@gmail.com>
2024-02-05 08:49:07 +0000

    need help

--- Form/login.js
+++ Form/login.js
@@ -1,8 +1,23 @@
-const form = document.getElementById("loginForm");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+const submit = document.getElementById("submit");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+    //e.preventDefault();
 });
+
+submit.addEventListener("click",() => {
+    submitForm();
+});
+
+
+function submitForm() {
+    // Get form data
+    const formData = new FormData(document.getElementById('loginForm'));
+
+    // Display form data in the result div
+    const resultDiv = document.getElementById('formResult');
+    resultDiv.innerHTML = '';
+
+    for (const [key, value] of formData.entries()) {
+      resultDiv.innerHTML += `<p><strong>${key}:</strong> ${value}</p>`;
+    }
+  }
